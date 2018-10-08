4c2155cf0aba000d0c867633148e092d5fd6f502
Sir Eraisuithon <mj@lpj-software.dk>
2018-10-08 11:35:33 +0200

    Fixed a potential crash if only one argument was given on program startup

--- src/matador/Matador.java
+++ src/matador/Matador.java
@@ -2,11 +2,12 @@ package matador;
 
 public class Matador {
 	public static void main(String[] args) {
-		boolean debugMode = args.length == 0;
-		int debugPlayers = 2;
-		int debugAIPlayers = 4;
+		boolean defaultMode = args.length == 0;
+		int defaultPlayers = 2;
+		boolean defaultAIMode = !(args.length == 2);
+		int defaultAIPlayers = 0;
 		
-		MatadorMain.init(debugMode ? debugPlayers : Integer.valueOf(args[0]), debugMode ? debugAIPlayers : Integer.valueOf(args[1]));
+		MatadorMain.init(defaultMode ? defaultPlayers : Integer.valueOf(args[0]), defaultAIMode ? defaultAIPlayers : Integer.valueOf(args[1]));
 		MatadorMain.startGameLoop();
 		MatadorMain.stop();
 	}
