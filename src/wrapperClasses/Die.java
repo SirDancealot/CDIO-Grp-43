a587a78a86a13b781d14e5d54b721997fa0af7b3
SirDancealot <mj@lpj-software.dk>
2018-10-08 02:05:26 +0200

    Made the bare requirements for the Die class

--- src/wrapperClasses/Die.java
+++ src/wrapperClasses/Die.java
@@ -1,5 +1,26 @@
 package wrapperClasses;
 
-public class Die {
+import util.CustomRandom;
 
+public class Die {
+	int size;
+	int faceValue;
+	
+	public Die(int size) {
+		this.size = size;
+		roll();
+	}
+	
+	public int roll() {
+		faceValue = CustomRandom.randInt(size);
+		return faceValue;
+	}
+	
+	public int getFaceValue() {
+		return faceValue;
+	}
+	
+	public void setFaceValue(int faceValue) {
+		this.faceValue = faceValue;
+	}
 }
