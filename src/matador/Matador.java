753e43a28a9e6b0338165498f9c354aa279e1c42
SirDancealot <mj@lpj-software.dk>
2018-10-08 02:00:30 +0200

    made the basic layout of AIs possible

--- src/matador/Matador.java
+++ src/matador/Matador.java
@@ -4,8 +4,9 @@ public class Matador {
 	public static void main(String[] args) {
 		boolean debugMode = args.length == 0;
 		int debugPlayers = 2;
+		int debugAIPlayers = 4;
 		
-		MatadorMain.init(debugMode ? debugPlayers : Integer.valueOf(args[0]));
+		MatadorMain.init(debugMode ? debugPlayers : Integer.valueOf(args[0]), debugMode ? debugAIPlayers : Integer.valueOf(args[1]));
 		MatadorMain.startGameLoop();
 		MatadorMain.stop();
 	}
--- src/matador/MatadorMain.java
+++ src/matador/MatadorMain.java
@@ -9,17 +9,27 @@ public class MatadorMain {
 	private static boolean playing = true;
 	private static Player[] players;
 	
-	public static void init(int numPlayers) {
-		players = new Player[numPlayers];
-		for (int i = 0; i < players.length; i++) {
-			System.out.println("Indtast navn på spiller " + (i + 1) + ": ");
+	public static void init(int numPlayers, int AIs) {
+		players = new Player[numPlayers + AIs];
+		for (int i = 0; i < numPlayers; i++) {
+			System.out.print("Indtast navn på spiller " + (i + 1) + ": ");
 			players[i] = new Player(sc.nextLine());
+		}
+		for (int i = 0; i < AIs; i++) {
+			players[numPlayers + i] = new Player();
 		}
 	}
 	
 	public static void startGameLoop() {
+		int turns = 1;
+		int numPlayer = 0;
 		while (playing) {
+			players[numPlayer].promptPlayer("test");
 			
+			if (++numPlayer >= players.length) {
+				turns ++;
+				numPlayer = 0;
+			}
 		}
 	}
 	
