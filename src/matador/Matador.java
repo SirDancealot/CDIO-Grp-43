c8ed7a001b41460aa3cb2d956d64eb604c42805d
Sir Eraisuithon <mj@lpj-software.dk>
2018-10-08 19:03:24 +0200

    Fixed a bug with an IOException and set default AIs to two

--- src/matador/Matador.java
+++ src/matador/Matador.java
@@ -1,11 +1,13 @@
 package matador;
 
+import java.io.IOException;
+
 public class Matador {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		boolean defaultMode = args.length == 0;
 		int defaultPlayers = 2;
 		boolean defaultAIMode = !(args.length == 2);
-		int defaultAIPlayers = 0;
+		int defaultAIPlayers = 2;
 		
 		MatadorMain.init(defaultMode ? defaultPlayers : Integer.valueOf(args[0]), defaultAIMode ? defaultAIPlayers : Integer.valueOf(args[1]));
 		MatadorMain.startGameLoop();
