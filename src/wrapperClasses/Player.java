a7ac0e5cb1f4ff3e8d370550bf61ad8c98870ffe
SirDancealot <mj@lpj-software.dk>
2018-10-08 02:06:26 +0200

    made the basics of the Player class

--- src/wrapperClasses/Player.java
+++ src/wrapperClasses/Player.java
@@ -1,7 +1,33 @@
 package wrapperClasses;
 
 public class Player {
+	private static int aiNum = 1;
+	private String name;
+	private boolean isAI = false;
+	
 	public Player(String name) {
-		
+		this.name = name;
+	}
+
+	public Player() {
+		this.name = "AI " + aiNum;
+		aiNum++;
+		this.isAI = true;
+	}
+	
+	public void setAI(boolean isAI) {
+		this.isAI = isAI;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void promptPlayer(String msg) {
+		if (isAI) {
+			System.out.println(name);
+		} else {
+			System.out.println(name);
+		}
 	}
 }
