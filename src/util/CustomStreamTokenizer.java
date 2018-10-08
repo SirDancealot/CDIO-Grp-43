2ddb0c84d1ce631f7d0ac81fd8db6f13a6fee6a1
Sir Eraisuithon <mj@lpj-software.dk>
2018-10-08 13:37:50 +0200

    Addd a wrapper-class for the StreamTokenizer for improved usage functions

---
+++ src/util/CustomStreamTokenizer.java
@@ -0,0 +1,35 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+
+public class CustomStreamTokenizer {
+	private static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+	public static final int TT_EOF = StreamTokenizer.TT_EOF;
+	public static final int TT_EOL = StreamTokenizer.TT_EOL;
+	public static final int TT_WORD = StreamTokenizer.TT_WORD;
+	public static final int TT_NUMBER = StreamTokenizer.TT_NUMBER;
+	
+	
+	public static void initTokenizer() {
+		st.eolIsSignificant(true);
+	}
+	
+	private int nextToken() throws IOException {
+		return st.nextToken();
+	}
+	
+	private void nextLine() throws IOException {
+		while (st.nextToken() != StreamTokenizer.TT_EOL) {
+		}
+	}
+	
+	public String nextString() throws IOException {
+		while (nextToken() != StreamTokenizer.TT_WORD) {
+			nextLine();
+		}
+		return st.sval;
+	}
+}
