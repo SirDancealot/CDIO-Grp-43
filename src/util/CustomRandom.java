bfea5d67a72b60111563733f38be1edebf011883
SirDancealot <mj@lpj-software.dk>
2018-10-08 02:20:08 +0200

    added a description to the custom random methods

--- src/util/CustomRandom.java
+++ src/util/CustomRandom.java
@@ -5,10 +5,19 @@ import java.util.Random;
 public class CustomRandom {
 	private static Random rand = new Random(System.currentTimeMillis());
 	
+	/**
+	 * @param maxNum
+	 * @return returns a random whole number between 0 and maxNum, both inclusive
+	 */
 	public static int randInt(int maxNum) {
 		return rand.nextInt(maxNum + 1);
 	}
 	
+	/**
+	 * @param low
+	 * @param high
+	 * @return returns a random whole number between low and high, both inclusive
+	 */
 	public static int randIntRange(int low, int high) {
 		return rand.nextInt(high + 1 - low) + low;
 	}
