8403383a09d7f5fd808a7a4bb4790eaddd0b6b26
PomFritzen <nicklas.fritzen@hotmail.com>
2018-11-02 12:00:07 +0100

    made english language file for player class

--- src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/Player.java
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/Player.java
@@ -81,34 +81,34 @@ public class Player {
 		if (DiceCup.isSameAndNum(1)) //If both dice shows 1 score gets reset
 			score = 0;
 		if (isAI) { //If player is an AI rolls automatically
-			System.out.println(name + " is rolling their dice");
+			System.out.println(name + " is rolling their dice"); //tag: playerRolling
 		} else { //If player is an actual player it waits for an input from the player
-			System.out.print("It's your turn to roll " + name + " press enter to roll");
+			System.out.print("It's your turn to roll " + name + " press enter to roll"); //tag: turnRoll //tag: enterRoll
 			CustomStreamTokenizer.waitForInput();
 		}
 		//Prints information to the player
-		System.out.println(name + " rolled " + DiceCup.getDiceStringValues() + " for a total of " + DiceCup.getDiceIntValues());
-		System.out.println(name + " now has a total score of " + score);
+		System.out.println(name + " rolled " + DiceCup.getDiceStringValues() + " for a total of " + DiceCup.getDiceIntValues()); //tag: playerRolled //tag: rolledResult
+		System.out.println(name + " now has a total score of " + score); //tag: playerTotalScore
 		if (DiceCup.isSameAndNum(1)) { //Prints information to player if their score got resat
-			System.out.println(name + " rolled two one's and gets their score reset to 0");
+			System.out.println(name + " rolled two one's and gets their score reset to 0"); //tag: doubleOneReset
 			lastOverWinscore = false;
 		}
 		calcHasWon(); //calculates whether the player has won now
 		if (DiceCup.isSameAndNum(6)) { 
 			//If both dice rolled a 6  it checks for if they rolled two sixes last time and if they did they won
 			if (lastTwoSixes) {
-				System.out.println(name + " has rolled two sixes twice in a row, and hereby wins the game");
+				System.out.println(name + " has rolled two sixes twice in a row, and hereby wins the game"); //tag: sixesWin
 				hasWon = true;
 			}
 			else {
-				System.out.println(name + " has rolled two sixes in one roll, if they do it again they win the game");
+				System.out.println(name + " has rolled two sixes in one roll, if they do it again they win the game"); //tag: sixesAlmostWin
 				lastTwoSixes = true;
 			}
 		} else
 			lastTwoSixes = false;
 		
 		if (DiceCup.isSame() && !hasWon) { //gives player an extra turn if they haven't won, and they rolled two identical 
-			System.out.println(name + " rolled two identical dice and get's another roll");
+			System.out.println(name + " rolled two identical dice and get's another roll"); //tag: additionalRoll
 			playerRollDice();
 		}
 	}
