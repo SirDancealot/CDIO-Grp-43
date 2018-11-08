6a013a6c1e579f8ffad3ee80b8c08d4bdf46a174
SirDancealot <mj@lpj-software.dk>
2018-11-08 20:56:12 +0100

    Started implementing timer to controll game

--- src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/GUI/MatodorGUI.java
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/GUI/MatodorGUI.java
@@ -482,5 +482,4 @@ public class MatodorGUI{
 
         }
         }
-
 }
---
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/util/Timer.java
@@ -0,0 +1,16 @@
+package dk.dtu.CDIT_Grp_43_matador.matador.util;
+
+public class Timer {
+	private int missingTicks;
+	private long last;
+	private int tps;
+	private long dMs;
+	
+	public Timer() {
+		
+	}
+	
+	public void update() {
+
+	}
+}
