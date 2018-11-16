93583fc85087b49d3406b280e571d7c8fd2ea04c
PomFritzen <nicklas.fritzen@hotmail.com>
2018-11-16 11:00:14 +0100

    ChanceCard classes and txt files created

---
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/ChanceCard.java
@@ -0,0 +1,5 @@
+package main.java.dk.dtu.CDIT_Grp_43_matador.matador.entity;
+
+public class ChanceCard {
+
+}
---
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/wraperClasses/ChanceCardWrapper.java
@@ -0,0 +1,4 @@
+package main.java.dk.dtu.CDIT_Grp_43_matador.matador.wraperClasses;
+
+public class ChanceCardWrapper {
+}
