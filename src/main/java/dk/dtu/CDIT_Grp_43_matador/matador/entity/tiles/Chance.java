1b9b2a3691fc570017df21c3afa004abf664061e
Magnus <magnus.brok@db-data.dk>
2018-11-19 14:50:00 +0100

    Java documentation for GoToJail, FreeParking and Chance

--- src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/tiles/Chance.java
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/tiles/Chance.java
@@ -5,10 +5,21 @@ import dk.dtu.CDIT_Grp_43_matador.matador.entity.Tile;
 
 public class Chance extends Tile {
 
+    /**
+     * representing the 4 chance tiles on the board.
+     * @param tilename parsed to superclass constructor {@code Tile}.
+     * @param tileinfo parsed to superclass constructor {@code Tile}.
+     */
     public Chance (String tilename, String tileinfo) {
         super (tilename, tileinfo);
 
     }
+
+    /**
+     * (NOT FINISHED) Draws a chance card, and gives the effect of the card.
+     * @param p The current player.
+     * @return returns true if everything goes right, ergo the current player hasn't lost.
+     */
     public boolean landOnTile(Player p) {
         // insert drawChanceCard method
         return true;
--- src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/tiles/FreeParking.java
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/tiles/FreeParking.java
@@ -4,11 +4,21 @@ import dk.dtu.CDIT_Grp_43_matador.matador.entity.Player;
 import dk.dtu.CDIT_Grp_43_matador.matador.entity.Tile;
 
 public class FreeParking extends Tile {
-
+    /**
+     * The basic setup for the Freeparking tile. It doesn't do anything at the moment,
+     * but we've made it for reference and if we wish to add rules to it in the future.
+     * @param tilename parsed to superclass constructor {@code Tile}.
+     * @param tileinfo parsed to superclass constructor {@code Tile}.
+     */
     public FreeParking(String tilename, String tileinfo) {
         super (tilename, tileinfo);
     }
 
+    /**
+     * Method used when the player lands on a tile. Doesn't do anything at the moment.
+     * @param p The current player.
+     * @return Returns true if the method goes as planned, meaning that the player hasn't lost.
+     */
     public boolean passedTile (Player p) {
         return true;
     }
