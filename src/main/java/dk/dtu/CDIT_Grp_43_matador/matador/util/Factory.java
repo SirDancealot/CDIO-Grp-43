c835fb08519a5b43bc618aa6921c28a0bd6379c0
Fipzz <Philiprumle@gmail.com>
2018-11-19 16:09:27 +0100

    Create Factory.java

---
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/util/Factory.java
@@ -0,0 +1,9 @@
+package dk.dtu.CDIT_Grp_43_matador.matador.util;
+import dk.dtu.CDIT_Grp_43_matador.matador.entity.Player;
+
+import static dk.dtu.CDIT_Grp_43_matador.matador.util.CustomStreamTokenizer.*;
+
+public class Factory {
+
+
+}
