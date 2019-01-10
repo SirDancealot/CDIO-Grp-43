dc1dd1db18b394e263bc94059f7857e116b8eb8a
Anders <s1851465@student.dtu.dk>
2019-01-10 19:30:41 +0100

    outcast for first iteration for move to closest ship chancecard.

---
+++ src/main/java/dk/dtu/CDIT_Grp_43_matador/matador/entity/cardEffects/MoveToClosestShipEffect.java
@@ -0,0 +1,39 @@
+package dk.dtu.CDIT_Grp_43_matador.matador.entity.cardEffects;
+
+import dk.dtu.CDIT_Grp_43_matador.matador.entity.Player;
+import dk.dtu.CDIT_Grp_43_matador.matador.entity.tiles.Tile;
+
+public class MoveToClosestShipEffect extends CardEffect {
+
+    public MoveToClosestShipEffect() {
+    }
+
+    @Override
+    public boolean useEffect(Player p) {
+        int shortestDistance = Integer.MAX_VALUE;
+        int[] shipPosition = new int[matchingTiles.length];
+        for (int i = 0; i < matchingTiles.length; i++) {
+            shipPosition[i] += matchingTiles.getTileIndex(i);
+        }
+        for (int i = 0; i < shipPosition.length; i++) {
+            int distance = shipPosition[i] - p.getCurrPos();
+            if(distance < 0)
+                distance += 40;
+            if(distance < shortestDistance) {
+                shortestDistance = distance;
+            }
+        }
+        p.move(shortestDistance);
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public String printEffect(Player p) {
+        return null;
+    }
+}
