ee22470d3a01e3f63c9d24d719a1386e3c901c4c
PomFritzen <nicklas.fritzen@hotmail.com>
2019-01-15 11:50:28 +0100

    Tax fixes and tests

--- src/test/java/dk/dtu/CDIT_Grp_43_matador/jUnitTest/testAfTax.java
+++ src/test/java/dk/dtu/CDIT_Grp_43_matador/jUnitTest/testAfTax.java
@@ -1,19 +1,29 @@
 package dk.dtu.CDIT_Grp_43_matador.jUnitTest;
 
+import dk.dtu.CDIT_Grp_43_matador.matador.GameController;
+import dk.dtu.CDIT_Grp_43_matador.matador.Main;
 import dk.dtu.CDIT_Grp_43_matador.matador.entity.Player;
 import dk.dtu.CDIT_Grp_43_matador.matador.entity.tiles.Tax;
+import dk.dtu.CDIT_Grp_43_matador.matador.wraperClasses.GameBoard;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class testAfTax {
+    @BeforeEach
+    public void init() {
+
+    }
 
     @Test
-    public void landOnTile() {
+    public void landOnTileTaxFlat() {
 
         Player p = new Player("testPlayer",1500);
         Tax taxTile = new Tax("","type:Tax;money:100;name:Tax Flat",38);
-
 
         System.out.println(p.getScore());
 
@@ -23,8 +33,9 @@ class testAfTax {
 
        assertTrue(1400 == p.getScore());
 
-    }
 
+    }
+    
     @Test
     public void printLandOn() {
     }
