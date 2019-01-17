24cfc02659bcbfea3a4b761735d85363b71cc2fe
andi365 <anders1998@live.dk>
2019-01-17 13:23:41 +0100

    Added text for the different tests
    Small fixes to print method needed in matadorlegateffect

--- src/test/java/dk/dtu/CDIT_Grp_43_matador/jUnitTest/cardEffectTest/MovePlayerEffectsTest.java
+++ src/test/java/dk/dtu/CDIT_Grp_43_matador/jUnitTest/cardEffectTest/MovePlayerEffectsTest.java
@@ -21,11 +21,17 @@ class MovePlayerEffectsTest {
         }
         Player p = new Player("Heidi", 1500);
         MovePlayerEffect movePlayer = new MovePlayerEffect(-3);
+        System.out.println("\n---- MovePlayerTest ----");
+        System.out.println("---- placering før test ---- \n" + p + " placering: " + p.getCurrPos());
+        System.out.println(movePlayer.print(p));
         movePlayer.useEffect(p);
+        System.out.println("---- placering efter test ---- \n" + p + " placering: " + p.getCurrPos());
         assertTrue(p.getCurrPos() == 37);
         MovePlayerEffect movePlayer1 = new MovePlayerEffect(3);
+        System.out.println(movePlayer1.print(p));
         movePlayer1.useEffect(p);
         assertTrue(p.getCurrPos() == 0);
+        System.out.println("---- placering efter test ---- \n" + p + " placering: " + p.getCurrPos());
     }
 
     @Test
@@ -37,10 +43,17 @@ class MovePlayerEffectsTest {
         }
         Player p = new Player("Malte", 1500);
         MovePlayerToEffect movePlayerTo = new MovePlayerToEffect("Roskildevej");
+        System.out.println("\n---- MovePlayerToTest ----");
+        System.out.println("---- placering før test ---- \n" + p + " placering: " + p.getCurrPos());
+        System.out.println(movePlayerTo.print(p));
         movePlayerTo.useEffect(p);
+        System.out.println("---- placering efter test ---- \n" + p + " placering: " + p.getCurrPos());
+
         assertTrue(p.getCurrPos() == 6);
         MovePlayerToEffect movePlayerTo1 = new MovePlayerToEffect("Valby Langgade");
+        System.out.println(movePlayerTo1.print(p));
         movePlayerTo1.useEffect(p);
         assertTrue(p.getCurrPos() == 8);
+        System.out.println("---- placering efter test ---- \n" + p + " placering: " + p.getCurrPos());
     }
 }
